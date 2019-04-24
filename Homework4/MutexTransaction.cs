8d8d637da9965abde812f076868d275a95a155cf
Viacheslav Holovnov <viacheslav.holovnov@globallogic.com>
2019-04-24 14:04:33 +0300

    Add project files.

---
+++ Homework4/MutexTransaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    public class MutexTransaction : ITransactionManager
+    {
+        public bool BeginTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CommitTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RollbackTransaction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Homework4/TransactionManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Homework4
+{
+    public interface ITransactionManager
+    {
+        bool BeginTransaction();
+
+        bool CommitTransaction();
+
+        bool RollbackTransaction();
+    }
+}
